eff3f64a0133e46bf7285908f46e0d0988dfcc39
lokwey-dev21 <muizoyetola8@gmail.com>
2025-01-02 22:00:32 +0100

    cors bug solved

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,9 +3,6 @@ import axios from "axios";
 const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_PORT,
   withCredentials: true,
-  headers: {
-    'Content-Type': 'application/json'
-  }
 });
 
 export default axiosInstance;
